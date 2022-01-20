80f3d5cc54083214afdb15fd9117bcc7405c04d9
Alan Pečenković <alanquad@yahoo.com>
2022-01-20 15:16:36 +0100

    Configuring custom Input so it can accept properties like keyboardType=number-pad

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -10,7 +10,14 @@ const StartGameScreen = (props) => {
       <Text style={styles.title}>Start a new Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a number</Text>
-        <Input style={styles.input} />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title="Reset" color={Colors.accent} onPress={() => {}} />
