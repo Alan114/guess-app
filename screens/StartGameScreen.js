893f0ad322d85bd7c514db9b968039013c43e152
Alan Pečenković <alanquad@yahoo.com>
2022-01-20 12:17:46 +0100

    adding View Text and Buttons to StartGameScreen and arranging them

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.gameScreen}>
-      <Text>Start a new Game!</Text>
+      <Text style={styles.title}>Start a new Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -12,9 +20,23 @@ const StartGameScreen = (props) => {
 const styles = StyleSheet.create({
   gameScreen: {
     flex: 1,
-    padding: 10,
+    padding: 20,
     alignItems: "center",
     backgroundColor: "whitesmoke",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
 
