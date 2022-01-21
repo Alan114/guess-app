a631ecec1812455c7876df390834979008f0f2a3
Alan Pečenković <alanquad@yahoo.com>
2022-01-21 15:10:07 +0100

    conditionally rendering StartGameScreen or GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -16,12 +18,29 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
-  return <View style={styles.gameScreen}>{Math.floor(Math.random * 100)}</View>;
+  return (
+    <View style={styles.gameScreen}>
+      <Text>Opponent's guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="HIGHER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   gameScreen: {
-    textAlign: "center",
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: "80%",
   },
 });
 export default GameScreen;
